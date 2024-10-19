80cc87fe71fd9e852032f52ba9dbcf75078ea425
AALOK SHAW <115942969+shawaalok2003@users.noreply.github.com>
2024-10-20 01:06:08 +0530

    new

--- frontend/src/components/Gallery.js
+++ frontend/src/components/Gallery.js
@@ -1,31 +1,27 @@
 import React from 'react';
-import { Carousel } from 'react-bootstrap'; // Import Carousel from React Bootstrap
 import './Gallery.css'; // Import the CSS file
 
 const Gallery = () => {
   const images = [
-    '/images/pexels-akash-2906350-5060587.jpg',
+    '/images/pexels-akash-2906350-5060587.jpg', // Replace with your image paths
     '/images/pexels-cottonbro-5416013.jpg',
     '/images/WhatsApp Image 2024-10-03 at 14.52.56 (1).jpeg',
     '/images/WhatsApp Image 2024-10-03 at 14.52.56.jpeg',
     '/images/WhatsApp Image 2024-10-03 at 14.52.57 (1).jpeg',
     '/images/WhatsApp Image 2024-10-03 at 14.52.57.jpeg',
+    
   ];
 
   return (
     <div className="gallery-container">
       <h1>Gallery</h1>
-      <Carousel>
+      <div className="gallery-grid">
         {images.map((image, index) => (
-          <Carousel.Item key={index}>
-            <img
-              className="d-block w-100"
-              src={image}
-              alt={`Gallery item ${index + 1}`}
-            />
-          </Carousel.Item>
+          <div className="gallery-item" key={index}>
+            <img src={image} alt={`Gallery item ${index + 1}`} />
+          </div>
         ))}
-      </Carousel>
+      </div>
     </div>
   );
 };
